896b5a5eaf72c6772ff9a1a10c942855a7e53f41
Berhan <57452604+berhanserin@users.noreply.github.com>
2022-01-05 12:09:59 +0300

    multer cropper file upload

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+const Buffer = require("buffer");
+const gm = require("gm").subClass({
+  imageMagick: true
+});
 const app = express();
 
 let PORT = 5000;
@@ -12,14 +18,64 @@ var storage = multer.diskStorage({
   filename: function (req, file, callback) {
     callback(
       null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      Date.now() + path.extname(file.originalname)
     );
   },
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({
+  storage: storage
+});
 
-app.post("/upload", upload.single("file"), function (req, res) {});
+const randomfileName = () => {
+  return "file_upload/" + Date.now() + ".png";
+}
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post("/upload", upload.single("file"), function (req, res) {
+  let file_width = 0;
+  let file_height = 0;
+  if (req.body.file) {
+    let filename = randomfileName();
+    var base64Data = req.body.file.replace(/^data:image\/png;base64,/, "");
+
+    require("fs").writeFile(filename, base64Data, 'base64', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    gm(filename).size((err, val) => {
+      file_width = val.width;
+      file_height = val.height;
+    });
+
+    gm(filename)
+      .composite("logo-remove.png")
+      .geometry("+0+0").write(filename, function (err) {
+        if (err) res.send(err)
+      });
+
+  } else {
+
+    gm(req.file.path).size((err, val) => {
+      file_width = val.width;
+      file_height = val.height;
+    });
+
+    gm(req.file.path)
+      .composite("logo-remove.png")
+      .geometry("+0+0").write(req.file.path, function (err) {
+        if (err) res.send(err)
+      });
+
+  }
+});
 
 app.post("/uploads", upload.array("photos", 2), function (req, res, next) {});
 
